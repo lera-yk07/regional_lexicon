4f8cf21ec6df26a42bc3135d54733f19e43c9b5c
Валерия Якимова <lerayakimova2407@gmail.com>
2026-01-14 22:34:17 +0500

    Register models in admin site

--- lexicon/admin.py
+++ lexicon/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Region, City, Word, Comparison
 
-# Register your models here.
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'word_count')
+    search_fields = ('name', 'description')
+    list_filter = ('created_at',)
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'region', 'population', 'founded_year', 'word_count')
+    search_fields = ('name', 'region__name')
+    list_filter = ('region',)
+
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    list_display = ('word', 'city', 'user', 'created_at', 'views_count')
+    search_fields = ('word', 'meaning', 'city__name')
+    list_filter = ('city__region', 'city', 'created_at')
+
+@admin.register(Comparison)
+class ComparisonAdmin(admin.ModelAdmin):
+    list_display = ('city1', 'city2', 'compared_at', 'common_words_count')
+    list_filter = ('compared_at',)
