c99eb164b0e44150e6de8a015b4f25c2f46c615b
Валерия Якимова <lerayakimova2407@gmail.com>
2026-01-19 17:37:31 +0500

    Add settings

--- regional_lexicon/settings.py
+++ regional_lexicon/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-hb4_o(8#thdoxb81w#=^b#%@f7@)w6d6x*4l+wg)%mu$jenm1*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lerayakimova.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
